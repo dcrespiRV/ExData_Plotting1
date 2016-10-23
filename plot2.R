2ff9b1aa8efc80c08dbac3c7bd5c3314aeef4e45
David Crespi <dcrespi@redventures.com>
2016-10-23 14:03:48 -0400

    All 4 plot.R files and all 4 plot.png files

---
+++ plot2.R
@@ -0,0 +1,26 @@
+# Import data
+library(tidyverse)
+fullData = read.table('household_power_consumption.txt',header = T,sep = ';')
+
+# Filter data for those dates and convert gap to numeric
+rawData = fullData %>% filter(Date %in% c('1/2/2007','2/2/2007')) %>% droplevels()
+rawData$Global_active_power = as.numeric(as.character(rawData$Global_active_power))
+
+# Date time formatting to combine into 1 column
+rawData$Date = as.character(rawData$Date)
+rawData$Date[which(rawData$Date == '1/2/2007')] = '2007-02-01'
+rawData$Date[which(rawData$Date == '2/2/2007')] = '2007-02-02'
+rawData$Date = as.Date(rawData$Date)
+rawData$Time = as.character(rawData$Time)
+rawData$Time = format(strptime(rawData$Time,'%T'),'%T')
+rawData$DateTime = as.POSIXct(paste(rawData$Date, rawData$Time), format="%Y-%m-%d %H:%M:%S")
+
+# Convert these to numeric
+rawData$Sub_metering_1 = as.numeric(as.character(rawData$Sub_metering_1))
+rawData$Sub_metering_2 = as.numeric(as.character(rawData$Sub_metering_2))
+rawData$Sub_metering_3 = as.numeric(as.character(rawData$Sub_metering_3))
+
+par(mfrow = c(1,1))
+plot(rawData$DateTime,rawData$Global_active_power,type = 'l',xlab = '',ylab = 'Global active power (kilowatts)')
+dev.copy(png,'plot2.png')
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,31 @@
+# Import data
+library(tidyverse)
+fullData = read.table('household_power_consumption.txt',header = T,sep = ';')
+
+# Filter data for those dates and convert gap to numeric
+rawData = fullData %>% filter(Date %in% c('1/2/2007','2/2/2007')) %>% droplevels()
+rawData$Global_active_power = as.numeric(as.character(rawData$Global_active_power))
+
+# Date time formatting to combine into 1 column
+rawData$Date = as.character(rawData$Date)
+rawData$Date[which(rawData$Date == '1/2/2007')] = '2007-02-01'
+rawData$Date[which(rawData$Date == '2/2/2007')] = '2007-02-02'
+rawData$Date = as.Date(rawData$Date)
+rawData$Time = as.character(rawData$Time)
+rawData$Time = format(strptime(rawData$Time,'%T'),'%T')
+rawData$DateTime = as.POSIXct(paste(rawData$Date, rawData$Time), format="%Y-%m-%d %H:%M:%S")
+
+# Convert these to numeric
+rawData$Sub_metering_1 = as.numeric(as.character(rawData$Sub_metering_1))
+rawData$Sub_metering_2 = as.numeric(as.character(rawData$Sub_metering_2))
+rawData$Sub_metering_3 = as.numeric(as.character(rawData$Sub_metering_3))
+
+par(mfrow = c(2,2))
+plot(rawData$DateTime,rawData$Global_active_power,type = 'l',xlab = '',ylab = 'Global active power (kilowatts)')
+plot(rawData$DateTime,rawData$Voltage,type = 'l',xlab = 'datetime',ylab = 'Voltage')
+plot(rawData$DateTime,rawData$Sub_metering_1,type = 'l',xlab = '',ylab = 'Energy sub metering')
+lines(rawData$DateTime,rawData$Sub_metering_2,col = 'red')
+lines(rawData$DateTime,rawData$Sub_metering_3,col = 'blue')
+plot(rawData$DateTime,rawData$Global_reactive_power,type = 'l',xlab = 'datetime',ylab = 'Global_reactive_power')
+dev.copy(png,'plot4.png')
+dev.off()
